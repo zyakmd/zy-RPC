e679d7124e1c9f1ef46e26d822badcf59f170e16
zyakmd <zy-wh@qq.com>
2024-05-10 21:19:23 +0800

    5.10

---
+++ RpcFramework/src/main/java/org/zy/rpc/protocol/serialization/HessianSerialization.java
@@ -0,0 +1,8 @@
+package org.zy.rpc.protocol.serialization;/**
+ * @package:        org.zy.rpc.protocol.serialization
+ * @author:     zyakmd
+ * @description:  TODO  
+ * @date:    2024/5/10 16:39
+ */ 
+public class HessianSerialization {
+}
---
+++ RpcFramework/src/main/java/org/zy/rpc/registry/RedisRegistry.java
@@ -0,0 +1,8 @@
+package org.zy.rpc.registry;/**
+ * @package:        org.zy.rpc.registry
+ * @author:     zyakmd
+ * @description:  TODO  
+ * @date:    2024/5/10 19:39
+ */ 
+public class RedisRegistry {
+}
